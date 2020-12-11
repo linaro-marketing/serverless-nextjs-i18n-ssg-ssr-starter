8ce1cf9b295561772af02c2ec3ecc01db894fc6c
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-12-11 12:59:46 +0000

    Modified the i18n config / added an additional example language / namespace

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,3 +1,4 @@
-export const languages = ['en', 'de'];
-
+// The supported languages that a locale JSON file exists for.
+export const languages = ['en', 'de', 'fr'];
+// The default language the app uses.
 export const defaultLanguage = 'en';
--- src/i18n/init.js
+++ src/i18n/init.js
@@ -1,8 +1,8 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 
 import { languages, defaultLanguage } from './config';
-
 const locales = Object.assign(
 	{},
 	...Object.keys(languages).map((index) => {
@@ -10,29 +10,47 @@ const locales = Object.assign(
 			[languages[index]]: {
 				translations: require('../locales/' + languages[index] + '/translation.json'),
 			},
+			[languages[index]]: {
+				routes: require('../locales/' + languages[index] + '/routes.json'),
+			},
 		};
 	}),
 );
-
-const lng =
-	typeof window !== 'undefined' && localStorage.getItem('lng')
-		? localStorage.getItem('lng')
-		: defaultLanguage;
-
-i18next.use(LanguageDetector).init({
-	lng: lng,
-	fallbackLng: defaultLanguage,
-	resources: locales,
-	ns: ['translations'],
-	defaultNS: 'translations',
-	returnObjects: true,
-	debug: false,
-	interpolation: {
-		escapeValue: false, // not needed for react!!
-	},
-	react: {
-		wait: true,
-	},
-});
+export const detectionOptions = {
+	// order and from where user language should be detected
+	order: ['navigator', 'path', 'localStorage', 'cookie', 'htmlTag'],
+	// keys or params to lookup language from
+	lookupQuerystring: 'lng',
+	lookupCookie: 'linaroResourcesHubLang',
+	lookupLocalStorage: 'linaroResourcesHubLang',
+	lookupSessionStorage: 'linaroResourcesHubLang',
+	lookupFromPathIndex: 0,
+	lookupFromSubdomainIndex: 0,
+	// cache user language on
+	caches: ['localStorage', 'cookie'],
+	excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+	// cookieDomain: 'cloudfront.net',
+	// optional set cookie options, reference:[MDN Set-Cookie docs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie)
+	cookieOptions: { path: '/', sameSite: 'strict' },
+};
+i18next
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		fallbackLng: defaultLanguage,
+		resources: locales,
+		ns: ['translations', 'routes'],
+		defaultNS: 'translations',
+		supportedLngs: languages,
+		returnObjects: true,
+		debug: false,
+		detection: detectionOptions,
+		interpolation: {
+			escapeValue: false, // not needed for react!!
+		},
+		react: {
+			wait: true,
+		},
+	});
 
 export default i18next;

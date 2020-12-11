949c41f599730be4edbc7550c5ae5ab2ca7eda1a
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-12-11 13:01:34 +0000

    Updated / redirect file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,18 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-
 import i18next from 'i18next';
 
-import { getSortedLangsData } from '../lib/lang';
-
-export default function Home({ allLangsData }) {
+/**
+ * Acts as a redirect for directing users to the current
+ * i18n subpath
+ */
+export default function Home() {
 	const router = useRouter();
-
 	useEffect(() => {
 		const { pathname } = router;
 		if (pathname == '/') {
 			router.push('/' + i18next.language.substring(0, 2));
 		}
 	});
-
 	return null;
 }
-
-/*
-export async function getStaticProps() {
-	const allLangsData = getSortedLangsData();
-	return {
-		props: {
-			allLangsData,
-		},
-	};
-}
-*/
